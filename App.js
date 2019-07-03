ff4b8590b2cdc89397bab06031429ceaa2099cf4
bobbyecho <raden.bambang@ralali.com>
2019-07-04 01:12:02 +0700

    test PR

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to RN</Text>
         <Text style={styles.instructions}>To get stated, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
