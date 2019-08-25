8706104d2c060dc3e16647f38b2ee923a9df7e11
bobbyecho <raden.bambang@ralali.com>
2019-08-25 17:10:54 +0700

    test

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+          
+          
+          
+import React, { Component } from 'react';
+import {Text, View} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+class App extends Component {
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R On your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  componentDidMount() {
 
-type Props = {};
-export default class App extends Component<Props> {
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    
+  }
+
+  exampleFoo() {
+    return "foo"
+  }
+
+  exampleBar() {
+    return "bar"
+  }
+  
   render() {
     return (
       <View style={styles.container}>
@@ -25,25 +29,10 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>To get stated, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App
+
+
